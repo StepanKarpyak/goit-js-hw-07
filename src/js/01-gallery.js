28fad68ec5accf0f11508cb33930abde7a4e4646
StepanKarpyak <stepan_karpyak@ukr.net>
2021-10-30 21:40:34 +0300

    поправка

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,45 +1,89 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryContainer = document.querySelector('.gallery');
+// const galleryContainer = document.querySelector('.gallery');
 
-const galleryItemsString = galleryItems.map(item =>
-    `<div class="gallery__item">
-    <a class="gallery__link" target="_self" href="${item.original}">
+// const galleryItemsString = galleryItems.map(item =>
+//     `<div class="gallery__item">
+//     <a class="gallery__link" target="_self" href="${item.original}">
+//     <img
+//         class="gallery__image"
+//         src="${item.preview}"
+//         data-source="${item.original}"
+//         alt="${item.description}"
+//     />
+//     </a>
+// </div>`
+// ).join('');
+
+// galleryContainer.insertAdjacentHTML("afterbegin", galleryItemsString);
+
+// galleryContainer.addEventListener("click", onGalleryClick);
+
+// function onGalleryClick(event) {
+//     event.preventDefault();
+
+//     const instance = basicLightbox.create(`
+//     <div class="modal">
+//     <img src="${event.target.dataset.source}" class="js-modal-img" width="800" height="600">
+//     </div>
+// `, {
+//         onShow: (instance) => {
+//             window.addEventListener("keydown", onKeyboardClick);
+//             function onKeyboardClick(event) {
+//                 if (event.code === 'Escape') {
+//                     instance.close();
+//                     window.removeEventListener('keydown', onKeyboardClick);
+//                 };
+//             };
+
+//             instance.element().querySelector('.js-modal-img').addEventListener("click", () => {
+//                 instance.close();
+//             });
+//         },
+//     }).show();
+// }
+
+const gallery = document.querySelector('.gallery');
+const createMarkup = images => {
+    return images
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
-        class="gallery__image"
-        src="${item.preview}"
-        data-source="${item.original}"
-        alt="${item.description}"
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
-    </a>
-</div>`
-).join('');
+  </a>
+</div>
+    `;
+        })
+        .join('');
+};
+const markup = createMarkup(galleryItems);
+const addMarkup = markup => {
+    gallery.insertAdjacentHTML('beforeend', markup);
+};
+addMarkup(markup);
 
-galleryContainer.insertAdjacentHTML("afterbegin", galleryItemsString);
+let instance = basicLightbox;
 
-galleryContainer.addEventListener("click", onGalleryClick);
+const onPress = e => {
+    if (e.key === 'Escape' && instance.visible()) {
+        instance.close();
+    }
+};
+const onClick = e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    } else {
+        instance = basicLightbox.create(`<img src="${e.target.dataset.source}">`);
+        instance.show();
+    }
+};
 
-function onGalleryClick(event) {
-    event.preventDefault();
-
-    const instance = basicLightbox.create(`
-    <div class="modal">
-    <img src="${event.target.dataset.source}" class="js-modal-img" width="800" height="600">
-    </div>
-`, {
-        onShow: (instance) => {
-            window.addEventListener("keydown", onKeyboardClick);
-            function onKeyboardClick(event) {
-                if (event.code === 'Escape') {
-                    instance.close();
-                    window.removeEventListener('keydown', onKeyboardClick);
-                };
-            };
-
-            instance.element().querySelector('.js-modal-img').addEventListener("click", () => {
-                instance.close();
-            });
-        },
-    }).show();
-}
+gallery.addEventListener('click', onClick);
+document.addEventListener('keydown', onPress);
